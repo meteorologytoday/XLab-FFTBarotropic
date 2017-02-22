299eb27ef8cb177ea983afa7ed34a67eab995a9c
meteorologytoday <meteorologytoday@github.com>
2017-02-22 14:48:03 +0800

    test

--- setup_env.sh
+++ setup_env.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-
+export PATH=`pwd`/bin:$PATH
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:`pwd`/lib
--- test/01-runtest/example.sh
+++ test/01-runtest/example.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
+mkdir input
+mkdir output
+
 makefield-Kuo2004.out
 main.out
-
-cat log | perl -lne 'if(/(.*\/)psi(.*?\.bin)/) { print "$_=>$1pres$2"; }' | invert_pres.out
-ls -v output | grep pres_step | awk '{ print "output/" $1 }' | find_min.out 1> output/pres_timeseries.txt
