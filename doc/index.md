fd9a45c523a9487a4fe174a69c779138f5811437
meteorologytoday <meteorologytoday@github.com>
2017-02-22 14:05:34 +0800

    Write doc/index.md

---
+++ doc/index.md
@@ -0,0 +1,20 @@
+# Documentation
+## Usage
+
+    > main.out <options>
+
+### options
+
+#### -I
+	Input folder in which initial binaries should be placed. Default value is **input**.
+#### -O
+	Output folder of all results: vorticity, divergence, streamfunction,... etc. Default value is **output**.
+
+#### -i
+	Initial binary file of vorticity.
+
+#### -s **TODO**
+	Specify source term of vorticity, still under construction.
+
+#### -f
+	Specify the fifo through which vorticity source will be given.
--- test/01-runtest_backup/example.sh
+++ test/01-runtest_backup/example.sh
@@ -4,13 +4,13 @@ fifo=vort_src_fifo
 #./bin/makefield-elliptic-vortex.out
 #./bin/makefield-const-vortex.out
 #./bin/makefield-gaussian.out
-./bin/makefield-Kuo2004.out
+makefield-Kuo2004.out
 
 rm -f $fifo
 mkfifo $fifo
 
-./bin/vort_src_input.out > $fifo &
-./bin/main.out -f${fifo}
+vort_src_input.out > $fifo &
+main.out -f${fifo}
 
-cat log | perl -lne 'if(/(.*\/)psi(.*?\.bin)/) { print "$_=>$1pres$2"; }' | ./bin/invert_pres.out
+cat log | perl -lne 'if(/(.*\/)psi(.*?\.bin)/) { print "$_=>$1pres$2"; }' | invert_pres.out
 ls -v output | grep pres_step | awk '{ print "output/" $1 }' | ./bin/find_min.out 1> output/pres_timeseries.txt
